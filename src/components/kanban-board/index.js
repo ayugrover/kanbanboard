4b3ae8df57b1c49525747c8e1694f1c319da5e20
Ayushi <ayushigrover001@gmail.com>
2021-10-27 02:18:16 +0530

    final commit

--- src/components/kanban-board/index.js
+++ src/components/kanban-board/index.js
@@ -7,26 +7,62 @@ export default class KanbanBoard extends Component {
     // Each task is uniquely identified by its name. 
     // Therefore, when you perform any operation on tasks, make sure you pick tasks by names (primary key) instead of any kind of index or any other attribute.
     this.state = {
+      taskName : " ",
       tasks: [
-            { name: '1', stage: 0 },
-            { name: '2', stage: 0 },
-        ],
-      
+            { name: 'Task1', stage: 0 },
+            { name: 'Task2', stage: 0 }, 
+          ]    
     };
-    this.stagesNames = ['Backlog', 'To Do', 'Ongoing', 'Done'];
-  }
 
+    this.stagesNames = ['Backlog', 'To Do', 'Ongoing', 'Done'];
+    this.timeout =  0;
+  }
+  
+  
   handleChange = (e) =>{
-   // const { tasks } = this.state;
-    const task = {name: e.target.value, stage: 0}
-    this.setState({
-        tasks: [...this.state.tasks, task]
-    });
-    
+    var val = e.target.value;
+    if(this.timeout) clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => {
+      this.setState({taskName : val}); 
+    }, 300);
   }
 
   createTask = () =>{
+    if(this.state.taskName !== " "){
+      const task = {name: this.state.taskName, stage: 0}
+      
+      this.setState({
+        tasks: [...this.state.tasks, task]
+      }); 
       console.log(this.state.tasks);
+    }
+  }
+
+  onClickBack = (e) =>{
+    var tasks = [...this.state.tasks];
+    var ele = tasks.findIndex(element => element.name === e.currentTarget.id);
+      if(tasks[ele].stage >= 1){
+          tasks[ele].stage -= 1;
+          this.setState({tasks});
+      }
+  }
+
+  
+  onClickForward = (e) =>{
+  var tasks = [...this.state.tasks];
+  var ele = tasks.findIndex(element => element.name === e.currentTarget.id);
+    if(tasks[ele].stage < 3){
+        tasks[ele].stage += 1;
+        this.setState({tasks});
+    }
+  };
+
+  onClickDelete = (e) =>{
+    var tasks = [...this.state.tasks];
+    var ele = tasks.findIndex(element => element.name === e.currentTarget.id);
+      tasks.splice(ele,1);
+      this.setState({tasks});
+    console.log("deleted");
   }
 
   render() {
@@ -36,6 +72,7 @@ export default class KanbanBoard extends Component {
     for (let i = 0; i < this.stagesNames.length; ++i) {
       stagesTasks.push([]);
     }
+
     for (let task of tasks) {
       const stageId = task.stage;
       stagesTasks[stageId].push(task);
@@ -60,13 +97,13 @@ export default class KanbanBoard extends Component {
                                       <div className="li-content layout-row justify-content-between align-items-center">
                                         <span data-testid={`${task.name.split(' ').join('-')}-name`}>{task.name}</span>
                                         <div className="icons">
-                                          <button className="icon-only x-small mx-2" data-testid={`${task.name.split(' ').join('-')}-back`}>
+                                          <button id={task.name} className="icon-only x-small mx-2" data-testid={`${task.name.split(' ').join('-')}-back`} onClick={this.onClickBack}>
                                             <i className="material-icons">arrow_back</i>
                                           </button>
-                                          <button className="icon-only x-small mx-2" data-testid={`${task.name.split(' ').join('-')}-forward`}>
+                                          <button id={task.name} className="icon-only x-small mx-2" data-testid={`${task.name.split(' ').join('-')}-forward`} onClick={this.onClickForward}>
                                             <i className="material-icons">arrow_forward</i>
                                           </button>
-                                          <button className="icon-only danger x-small mx-2" data-testid={`${task.name.split(' ').join('-')}-delete`}>
+                                          <button id={task.name} className="icon-only danger x-small mx-2" data-testid={`${task.name.split(' ').join('-')}-delete`} onClick={this.onClickDelete}>
                                             <i className="material-icons">delete</i>
                                           </button>
                                         </div>
